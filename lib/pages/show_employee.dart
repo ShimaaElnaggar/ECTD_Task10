b5dbff829fc3e996234b8d7229e4d2cdec6285e7
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-05-26 15:02:02 +0300

    check on Navigation process

--- lib/pages/show_employee.dart
+++ lib/pages/show_employee.dart
@@ -1,54 +1,66 @@
-
 import 'package:ectd_task10/models/employee_model.dart';
 import 'package:ectd_task10/pages/add_employee.dart';
 import 'package:ectd_task10/sql_dp.dart';
 import 'package:flutter/material.dart';
 
 class ShowEmployee extends StatefulWidget {
- List <EmployeeModel> employeeData = [];
- var sqlHelper = SqlHelper();
-  ShowEmployee({required this.employeeData,required this.sqlHelper,super.key});
+  List<EmployeeModel> employeeData ;
+  var sqlHelper = SqlHelper();
+
+  ShowEmployee(
+      {required this.employeeData, required this.sqlHelper, super.key});
 
   @override
   State<ShowEmployee> createState() => _ShowEmployeeState();
 }
 
 class _ShowEmployeeState extends State<ShowEmployee> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
-        title: const Text("Show Employee",style: TextStyle(color: Colors.white),),
-      ),
-      body: widget.employeeData.isEmpty? const Center(
-        child: Text(
-            " No Employee Added Yet!",
-          style: TextStyle(
-            color: Colors.deepPurple,
-            fontSize: 20,
-          ),
+        title: const Text(
+          "Show Employee",
+          style: TextStyle(color: Colors.white),
         ),
-      ):DataTable(
-        columns: const [
-          DataColumn(label: Text("ID")),
-          DataColumn(label: Text("Name")),
-          DataColumn(label: Text("Email")),
-          DataColumn(label: Text("Phone")),
-          DataColumn(label: Text("Address")),
-        ],
-        rows: buildRows(),
       ),
+      body: widget.employeeData.isEmpty
+          ? const Center(
+              child: Text(
+                " No Employee Added Yet!",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 20,
+                ),
+              ),
+            )
+          : DataTable(
+              columns: const [
+                DataColumn(label: Text("ID")),
+                DataColumn(label: Text("Name")),
+                DataColumn(label: Text("Email")),
+                DataColumn(label: Text("Phone")),
+                DataColumn(label: Text("Address")),
+              ],
+              rows: buildRows(),
+            ),
       floatingActionButton: FloatingActionButton(
-
-        onPressed: () {
-          Navigator.push(
+        onPressed: () async {
+          print("Button pressed. Navigating to AddEmployeePage...");
+          final result = await Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => AddEmployeePage(
-                    employeeData: widget.employeeData,
-                    sqlHelper: widget.sqlHelper,
-                  ))); },
+                        employeeData: widget.employeeData,
+                        sqlHelper: widget.sqlHelper,
+                      )));
+          if (result != null) {
+            print("Received result from AddEmployeePage: $result");
+          }
+          setState(() {});
+        },
         child: const Icon(Icons.add),
       ),
     );
